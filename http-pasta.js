4a9d8899e21e2426cd762b530746c9a7f05bc69f
Nick Niemeir <nick.niemeir@gmail.com>
2013-01-12 22:24:30 -0700

    init commit

---
+++ http-pasta.js
@@ -0,0 +1,69 @@
+// http-pasta.js
+var p = require('pasta')()
+
+function httpPasta (opts) {
+  // HTTP Functions
+
+  function errorHandler (res) {
+    var fired = false
+    return function (error, code) {
+      if (!fired) {
+        fired = true
+        res.statusCode = (code || 500)
+        res.end('Server ' + error)
+      } else {
+        p.log("Allready fired " + error)
+      }
+    }
+  }
+
+  function notyet (req, res) {
+    p.log('You called a route that is not yet implemented')
+    res.statusCode = 404
+    res.end('not found')
+  }
+
+
+  function redirect (loc) {
+    return function (req, res) {
+      res.statusCode = 302
+      res.setHeader('location', loc)
+      res.end()
+    }
+  }
+
+  function jsonCORS (req, res) {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    res.setHeader('Access-Control-Allow-Origin', '*')
+  }
+
+  function refuse (req, res, allowed) {
+    res.statusCode = 405
+    res.setHeader('Allow', allowed)
+    res.end('Method Not Allowed')
+  }
+
+  function dispatch (methods) {
+    return function (req, res) {
+      var intended = methods[req.method]
+      if (typeof intended === 'function') return intended.apply(null, arguments)
+      var _default = methods[_default]
+      if (typeof _default === 'function') return _default.apply(null, arguments)
+      return refuse(req, res, Object.keys(methods))
+    }
+  }
+
+  return { errorHandler: errorHandler
+    , eh: errorHandler
+    , notyet: notyet
+    , jsonCORS: jsonCORS
+    , redirect: redirect
+    , dispatch: dispatch
+    , refuse: refuse
+    }
+
+}
+
+if (module && module.exports) module.exports = httpPasta
+
---
+++ readme.rst
@@ -0,0 +1,8 @@
+===============================================================================
+HTTP Pasta
+===============================================================================
+
+A collection of req/res functions that I have found myself copy pastaing around.
+
+I am trying to separate the pasta into useful strands.
+
